eb89c824c01490b69839c2f44cfaf0b50ef1132a
vyomg <83865210+vyomg@users.noreply.github.com>
2022-06-21 14:41:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup() {
+    video =  createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(500, 500);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose, gotPoses');
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized!');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX +"noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX +"rightWristX = "+ rightWrist +"difference = " + difference);
+    }
+}
+
+function draw() {
+    background('#969A97');
+
+    document.getElementById("square_side").innerHTML = "width And Height of a Square will be =" + difference +"px";
+    Fill('F90093');
+    stroke('F90093');
+    square(noseX, noseY, difference);
+}
